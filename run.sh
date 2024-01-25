b0177a35a5b15bf71f8ae3455fb3fa85716758a1
thilinawishvekeerthi <thilinawishvekeerthi@outlook.com>
2024-01-26 02:23:28 +0530

    commiting pre-commit and test run.sh

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ TEST_REPORT=$REPORTING_DIR/test_report
 TEST_REPORT_HTML=$TEST_REPORT.html
 TEST_REPORT_XML=$REPORTING_DIR/test_report.xml
 
-pytest --disable-warnings --junit-xml=$TEST_REPORT_XML --html=$TEST_REPORT_HTML --self-contained-html --cov=$SOURCE_DIR --cov-report=html:$TEST_COV --cov-report=xml:$TEST_COV_XML --cov-report=term-missing test
+pytest -s -v --disable-warnings --junit-xml=$TEST_REPORT_XML --html=$TEST_REPORT_HTML --self-contained-html --cov=$SOURCE_DIR --cov-report=html:$TEST_COV --cov-report=xml:$TEST_COV_XML --cov-report=term-missing test
