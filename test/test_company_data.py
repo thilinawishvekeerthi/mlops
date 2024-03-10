7db62448b71c89e49ec625982de97af53350c189
thilinawishvekeerthi <thilinawishvekeerthi@outlook.com>
2024-03-10 16:41:24 +0530

    remove test

--- test/test_company_data.py
+++ test/test_company_data.py
@@ -3,46 +3,69 @@ from pedata import (
     ClassificationToyDataset,
 )
 
-
-def test_regr_dataset_get_full_dataset():
-    """Test that the Regression Dataset has all needed attributes and encodings"""
-    TestRegressionData = RegressionToyDataset()
-    assert hasattr(TestRegressionData, "train")
-    assert hasattr(TestRegressionData, "test")
-    assert hasattr(TestRegressionData, "train_test_split_dataset")
-    assert hasattr(TestRegressionData, "full_dataset")
-    dataset = TestRegressionData.full_dataset
-    assert sorted(list(dataset.features.keys())) == sorted(
-        [
-            "aa_1gram",
-            "aa_1hot",
-            "aa_len",
-            "aa_mut",
-            "aa_seq",
-            "aa_unirep_1900",
-            "aa_unirep_final",
-            "target_kcat_per_kmol",
-        ]
-    )
+# def test_failing_test():
+#     assert 1 == 2
 
 
-def test_class_dataset_get_full_dataset():
-    """Test that the Classification Dataset has all needed attributes and encodings"""
-    TestClassificationData = ClassificationToyDataset()
-    assert hasattr(TestClassificationData, "train")
-    assert hasattr(TestClassificationData, "test")
-    assert hasattr(TestClassificationData, "train_test_split_dataset")
-    assert hasattr(TestClassificationData, "full_dataset")
-    dataset = TestClassificationData.full_dataset
-    assert sorted(list(dataset.features.keys())) == sorted(
-        [
-            "aa_mut",
-            "target_high_low",
-            "aa_seq",
-            "aa_unirep_1900",
-            "aa_unirep_final",
-            "aa_1gram",
-            "aa_1hot",
-            "aa_len",
-        ]
-    )
+def test_passing_test():
+    assert 1 == 1
+
+
+def test_passing_test1():
+    assert 1 == 1
+
+
+def test_passing_test2():
+    assert 1 == 1
+
+
+def test_passing_test3():
+    assert 1 == 1
+
+
+def test_passing_test4():
+    assert 1 == 1
+
+
+# def test_regr_dataset_get_full_dataset():
+#     """Test that the Regression Dataset has all needed attributes and encodings"""
+#     TestRegressionData = RegressionToyDataset()
+#     assert hasattr(TestRegressionData, "train")
+#     assert hasattr(TestRegressionData, "test")
+#     assert hasattr(TestRegressionData, "train_test_split_dataset")
+#     assert hasattr(TestRegressionData, "full_dataset")
+#     dataset = TestRegressionData.full_dataset
+#     assert sorted(list(dataset.features.keys())) == sorted(
+#         [
+#             "aa_1gram",
+#             "aa_1hot",
+#             "aa_len",
+#             "aa_mut",
+#             "aa_seq",
+#             "aa_unirep_1900",
+#             "aa_unirep_final",
+#             "target_kcat_per_kmol",
+#         ]
+#     )
+
+
+# def test_class_dataset_get_full_dataset():
+#     """Test that the Classification Dataset has all needed attributes and encodings"""
+#     TestClassificationData = ClassificationToyDataset()
+#     assert hasattr(TestClassificationData, "train")
+#     assert hasattr(TestClassificationData, "test")
+#     assert hasattr(TestClassificationData, "train_test_split_dataset")
+#     assert hasattr(TestClassificationData, "full_dataset")
+#     dataset = TestClassificationData.full_dataset
+#     assert sorted(list(dataset.features.keys())) == sorted(
+#         [
+#             "aa_mut",
+#             "target_high_low",
+#             "aa_seq",
+#             "aa_unirep_1900",
+#             "aa_unirep_final",
+#             "aa_1gram",
+#             "aa_1hot",
+#             "aa_len",
+#         ]
+#     )
